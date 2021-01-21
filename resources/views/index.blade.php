29a776f63b989a735cf787d6824714a3b6da81f3
root <root@15ARE05.localdomain>
2021-01-21 15:13:29 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livewire Quickstart</title>
+    <livewire:styles />
+</head>
+<body>
+    <livewire:counter />
+    <livewire:scripts />
+</body>
+</html>
